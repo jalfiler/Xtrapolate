aa7e8021e72e9e14011833d82161782ff9c31a91
Andy Turner <81494303+turnerandy17@users.noreply.github.com>
2024-10-26 13:33:32 -0700

    Added GameManager Parent Class
    - Added GameManager Class
    - Added print statements for player experience

--- data/mainAT.py
+++ data/mainAT.py
@@ -9,6 +9,29 @@ from sklearn.metrics import mean_absolute_percentage_error
 from sklearn.metrics import mean_squared_error, r2_score
 
 
+# Arrays -----------------------------------------------------------------------------------------------------------------
+# Predefined dataset of topics
+topics = [
+    "Sales",
+
+]
+
+#  "Electric Vehicles",
+#  "Baby Names",
+
+difficulty = [
+    "Easy",
+    "Medium",
+    "Hard",
+]
+
+status = [
+    '1',
+    '2',
+]
+
+
+# Connect to Data --------------------------------------------------------------------------------------------------------
 file_url = 'https://raw.githubusercontent.com/T-Rex-chess/OMSBACapstone/main/data/sales_data_sample.csv'
 sales_data = pd.read_csv(file_url, encoding='ISO-8859-1') #Handles wider range of special char.
 sales_data_df = pd.DataFrame(sales_data) # create a dataframe of the sales data
@@ -23,6 +46,70 @@ print(sales_data_df.head())
 class BaseManager:
     def __init__(self, data):
         self.data = data
+
+
+
+# GameManager Parent Class ------------------------------------------------------------------------------------------------
+class GameManager:
+    def __init__(self, topics, difficulty, game_counter, game_status):
+        self.topics = topics
+        self.difficulty = difficulty
+        self.game_counter = 0
+        self.game_status = 1
+
+    def display_topics(self):
+        print("Available topics:")
+        for i, topic in enumerate(self.topics, start=1):
+            print(f"{i}. {topic}")
+
+    def get_topic_selection(self):
+        while True:
+            try:
+                choice = int(input("Please select a topic by entering the corresponding number: "))
+                if 1 <= choice <= len(self.topics):
+                    return self.topics[choice - 1]
+                else:
+                    print(f"Please enter a number between 1 and {len(self.topics)}.")
+            except ValueError:
+                print("Invalid input. Please enter a number.")
+
+    def display_difficulty(self):
+        print("Available Difficulties:")
+        for i, difficulty in enumerate(self.difficulty, start=1):
+            print(f"{i}. {difficulty}")
+
+    def get_difficulty_selection(self):
+        while True:
+            try:
+                choice = int(input("Please select a difficulty by entering the corresponding number: "))
+                if 1 <= choice <= len(self.difficulty):
+                    return difficulty[choice - 1]
+                else:
+                    print(f"Please enter a number between 1 and {len(self.difficulty)}.")
+            except ValueError:
+                print("Invalid input. Please enter a number.")
+
+    # Asks the player if they want to continue playing
+    def update_game_status(self):
+        while True:
+            try:
+                choice = int(input("Would you like to play again? Enter 1 for Yes, 2 for No: "))
+                if choice == 1:
+                    return self.game_status
+                else:
+                    break
+            except ValueError:
+                print("Invalid input. Please enter a number.")
+
+
+    # Create a gameplay loop counter (ie a round) that increments after player has completed first round
+    def increment_game_counter(self):
+        while self.game_status == 1: 
+            print('Completed Round \n', self.game_counter)
+            self.game_counter += 1 
+            print('Would you like to play again?')
+    
+
 
 
 
@@ -58,6 +145,21 @@ class DataLoader(BaseManager):
             pd.DataFrame: A DataFrame containing summary statistics.
         """
         return self.data.describe()
+    
+
+    '''
+    AT: - We should create functions to filter the dataset (maybe based on country or timeframe?)
+    AT: - And we should prompt the player to select which filter they want to apply
+    AT: starting the functions as below...
+
+    def get_unique_values(self):
+        unique_values = df['column_name'].unique()
+        return unique_values
+    
+    def filter_data(self):
+        filtered_df = df[df['column_name'].isin(unique_values)]
+        return filtered_df
+    '''
 
 
 
@@ -148,8 +250,9 @@ intercept = model.get_intercept()
 
 
 
-# ----- RUN THE PROGRAM !!! -------------------------------------------------------------------------------------
+# ----- RUN THE BACKEND PROGRAM !!! -----------------------------------------------------------------------------
 # Call the DataLoader Class -------------------------------------------------------------------------------------
+print('Backend code testing/debugging beginning')
 print("\n")
 print("Beginning program execution \n")
 print("Calling the DataLoader Class \n")
@@ -192,8 +295,60 @@ print("MSE: ", mse)
 print("r2: ", r2)
 print("MAPE: ", mape)
 print("\n")
-print("Program Executed Successfully \n")
+print("Backend Program Executed Successfully \n \n \n")
+print("---------------------------------------------------- \n")
 
-# END -----------------------------------------------------------------------------------------------------------
 
 
+# END of BACKEND PROGRAM ----------------------------------------------------------------------------------------
+# ---------------------------------------------------------------------------------------------------------------
+# Eventually reshuffle all the code around so this makes sense with the backend program -------------------------
+# START GAMEPLAY LOOP -------------------------------------------------------------------------------------------
+print('Welcome to Xtrapolate: Gamified Data Science!')
+print("In this game, you will attempt to guess values based on some graphed data.")
+print("Are you ready to play? \n")
+
+# Initialize the game
+game = GameManager(topics, difficulty, 0, 1)
+
+# player selects difficulty
+game.display_difficulty()
+selected_difficulty = game.get_difficulty_selection()
+print(f"\nYou selected: {selected_difficulty}")
+
+# player selects topic
+game.display_topics()
+selected_topic = game.get_topic_selection()
+print(f"\nYou selected: {selected_topic}")
+
+# player filters the data
+# insert code here once filter functions working on dataset
+
+
+# Display the summary statistics of the data
+print("\n")
+print("Here are the summary statistics of the Sales Data: \n")
+summ_stats_df = DataLoader(sales_data_df)
+summ_stats_df.preprocess_data()
+summ_stats_df.handle_missing_values()
+summary_stats = summ_stats_df.get_summary_stats()
+print(summary_stats)
+
+
+print("\n")
+print('Here is a chart of the data:')
+# Display the chart here ----------------
+
+
+print("\n")
+print("Now you will guess the next five values of the data.")
+
+# Evaluate data and provide scores
+# insert code here 
+
+# Declare winner
+# insert code here 
+
+# Ask Player if they want to play next round
+print("\n")
+game.update_game_status()
